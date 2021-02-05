0d6024ffdce7c51cd30bf0c9e55a0ecaf98c190f
s-blais <stephen@s-blais.com>
2021-02-05 14:55:13 -0500

    commit tree 83a69190093150a7893c2adcf5c45c7debe96f62

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ export class App extends Component {
   render() {
     return (
       <div className="App">
-        <UserInput store={this.props.store}/>
+        <UserInput store={this.props.store} />
       </div>
     );
   }
--- src/components/UserInput.js
+++ src/components/UserInput.js
@@ -1,13 +1,41 @@
 import React, { Component } from 'react';
 
 class UserInput extends Component {
+
+  state = { username: '', hometown: '' }
+
+  handleChange = event => {
+    this.setState({ [event.target.name]: event.target.value })
+  }
+
+  handleSubmit = event => {
+    event.preventDefault()
+    this.props.store.dispatch({ type: 'ADD_USER', user: this.state })
+  }
+
   render() {
     return(
       <div>
-        user input
+        <form onSubmit={this.handleSubmit}>
+          <input type="text" 
+            name="username" 
+            onChange={this.handleChange} 
+            value={this.state.username} />
+          <input type="text" 
+            name="hometown" 
+            onChange={this.handleChange} 
+            value={this.state.hometown} />
+          <input type="submit" />
+        </form>
       </div>
     );
   }
 };
 
+
+// const mapDispatchToProps = dispatch => ({
+//   addUser: formData => dispatch ({ type: 'ADD_USER', user: formData })
+// })
+
+// export default connect(null, mapDispatchToProps)(UserInput);
 export default UserInput;
